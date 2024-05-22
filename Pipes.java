6f0fb462d02556971d186f36098221017232a09e
AhmedMohamed056 <hamadarayyan056@gmail.com>
2024-05-22 12:03:16 +0300

    pipes update

--- Pipes.java
+++ Pipes.java
@@ -2,21 +2,31 @@ import java.util.Scanner;
 
 public class Pipes {
     public static void main(String[] args) {
-        int[] arr={1000,500,100,25,10,5,1};
+        double[] arr={1000,500,100,25,10,5,1};
         System.out.println("enter number of meters: ");
         double scan=new Scanner(System.in).nextDouble();
         double scan1=scan*100;
         double resl=0.0;
-        int i;
-        for ( i=0;i<arr.length;i++){
-            resl+=arr[i];
-            if (scan1==resl){
-                System.out.println(resl);
-            }else {
-                i=0;
+        int lop;
+        if (scan1%1000==0) {
+             lop = (int) scan1 ;
+        }else {
+            lop=(int)scan1+1;
+        }
+        a:for (int i=0;i<=lop;i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (resl<scan1){
+                    resl += arr[j];
+                    if (resl>scan1){
+                        resl-=arr[j];
+                    }
+                }
+                if (resl==scan1){
+                    System.out.println(resl);
+                    break a;
+                }
+
             }
-
-
         }
     }
 }
